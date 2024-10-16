f2e6928754a7fdfcfd81f0ce6eed021caf4fe903
DocSneiderPlumbus <doc-sneider@hotmail.com>
2024-10-16 06:35:33 +0200

    t
    test

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--allow-code --xformers --api --no-half-vae --ckpt --dir C:\\stable-diffusion-checkpoints 
+set CUDA_VISIBLE_DEVICES=0
 
 call webui.bat
